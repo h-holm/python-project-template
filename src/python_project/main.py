8b55f0bedc05e17249bc86c547af1949faa36a25
Henrik Holm <holmhenrik93@gmail.com>
2025-03-22 02:43:52 +0100

    Enable logging to file via `--log-file-path` input argument

--- src/python_project/main.py
+++ src/python_project/main.py
@@ -58,24 +58,48 @@ def fibonacci(n: int) -> int:
 def main(
     nth_number: Annotated[int, typer.Argument(help="The nth Fibonacci number to compute.", min=0, envvar="NTH_NUMBER")],
     log_level: Annotated[LogLevel, typer.Option(help="Log level")] = LogLevel.INFO,
+    log_file_path: Annotated[Path | None, typer.Option(help="If provided, persist logs in this file.")] = None,
 ) -> None:
     """Log the `nth_number` of the Fibonacci sequence."""
     LOGGER.setLevel(log_level.upper())
-    start_timestamp = datetime.now(tz=UTC)
-    LOGGER.info(f"Script started at: {start_timestamp.strftime(TIMESTAMP_FORMAT)} ({UTC}).")
 
-    LOGGER.info(f"The {nth_number}{get_ordinal_suffix(nth_number)} Fibonacci number is: {fibonacci(nth_number)}.")
+    if log_file_path:
+        # Set up logging to file.
+        file_handler = logging.FileHandler(log_file_path)
+        if LOGGER.handlers and LOGGER.handlers[0].formatter:
+            # Use the same formatter as for the console output. The `._fmt` attribute of a `logging.Formatter` is pre-
+            # fixed with an underscore, indicating that it is intended for private use, but given the long history and
+            # widespread usage of the `logging` module, the attribute is unlikely to change. Hence, we use it here.
+            formatter = logging.Formatter(LOGGER.handlers[0].formatter._fmt, datefmt=TIMESTAMP_FORMAT)  # noqa: SLF001
+        else:
+            formatter = logging.Formatter(datefmt=TIMESTAMP_FORMAT)
+            LOGGER.warning("No console handler formatter was found. Using a default formatter for the file handler.")
+        file_handler.setFormatter(formatter)
+        LOGGER.addHandler(file_handler)
+
+    start_timestamp = datetime.now(tz=UTC)
+    LOGGER.info(f"Script started at {start_timestamp.strftime(TIMESTAMP_FORMAT)} ({UTC})")
+
+    LOGGER.info(f"The {nth_number}{get_ordinal_suffix(nth_number)} Fibonacci number is: {fibonacci(nth_number)}")
 
     end_timestamp = datetime.now(tz=UTC)
     time_elapsed_string = get_time_elapsed_string(end_timestamp - start_timestamp)
-    LOGGER.info(f"Script finished at {end_timestamp.strftime(TIMESTAMP_FORMAT)} ({UTC}).")
-    LOGGER.info(f"Time elapsed: {time_elapsed_string}.")
+    LOGGER.info(f"Script finished at {end_timestamp.strftime(TIMESTAMP_FORMAT)} ({UTC})")
+    LOGGER.info(f"Time elapsed: {time_elapsed_string}")
 
 
 if __name__ == "__main__":
     # Set up logging.
     logging.config.fileConfig(
-        Path(__file__).parents[0] / "logging.conf", {"datefmt": TIMESTAMP_FORMAT}, disable_existing_loggers=False
+        Path(__file__).parents[0] / "logging.conf",
+        {"datefmt": TIMESTAMP_FORMAT},
+        disable_existing_loggers=False,
     )
     LOGGER = logging.getLogger()
+
+    # Run the `typer` app.
     app()
+else:
+    # This block is executed when the module is imported. This is useful for testing purposes, as it allows us to
+    # import the functions and classes defined in this file without running the `typer` app.
+    LOGGER = logging.getLogger(__name__)
